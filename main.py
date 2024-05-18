cfd200492ffc4a65a7c6ccf514dbd36a758e206f
Marko <marko.parfeniuk.pm.2020@lpnu.ua>
2024-05-18 08:46:56 +0300

    update port

--- main.py
+++ main.py
@@ -182,4 +182,4 @@ def get_user_vocabulary():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
